4614f2bc86d46696ee8b331e57f6b9b11f81304d
your name <you@email.com>
2026-06-10 09:35:49 +0100

    debug database url

--- app/core/config.py
+++ app/core/config.py
@@ -1,41 +1,28 @@
 from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 load_dotenv()
 
 
 class Settings(BaseSettings):
-
-    # =========================
-    # PROJECT
-    # =========================
     PROJECT_NAME: str
 
-    # =========================
-    # DATABASE
-    # =========================
     DATABASE_URL: str
 
-    # =========================
-    # JWT
-    # =========================
     SECRET_KEY: str
-
     ALGORITHM: str
-
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    # =========================
-    # PAYSTACK
-    # =========================
     PAYSTACK_SECRET_KEY: str
-
     PAYSTACK_PUBLIC_KEY: str
+
     EMAIL_ADDRESS: str
     EMAIL_PASSWORD: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
 
 
 settings = Settings()
--- app/core/database.py
+++ app/core/database.py
@@ -4,7 +4,12 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+print("DATABASE_URL =", settings.DATABASE_URL)
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -17,9 +22,7 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
-
     finally:
         db.close()
