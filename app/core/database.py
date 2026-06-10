d9537c5d399aaa50f5f95eb320e5fb1444a41e16
your name <you@email.com>
2026-06-10 09:51:55 +0100

    database

--- app/core/database.py
+++ app/core/database.py
@@ -4,12 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-print("DATABASE_URL =", settings.DATABASE_URL)
-
-engine = create_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True
-)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -22,7 +17,9 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
+
     finally:
         db.close()
